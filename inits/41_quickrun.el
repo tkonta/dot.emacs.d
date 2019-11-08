d9fd044b9e4824e5a2a9eec8f23090b5ffce7fdb
Takumi Konta <takumi.konta@litalico.co.jp>
2019-11-08 19:30:47 +0900

    quickrunのリージョン実行自動判定

--- inits/41_quickrun.el
+++ inits/41_quickrun.el
@@ -1,6 +1,13 @@
 ;; バッファ内でコード実行
 (use-package quickrun
   :config
-  (global-set-key (kbd "<f5>") 'quickrun)
-)
+  ;;リージョン実行自動判別
+  (defun quickrun-sc (start end)
+	(interactive "r")
+	(if mark-active
+		(quickrun :start start :end end)
+	  (quickrun)))
+
+  (global-set-key (kbd "<f5>") 'quickrun-sc)
+  )
 
