9a5bb79a8e90f9777caf2a524fa1d26a9ea6dc57
takumi konta <tak@taks-MBP-2019.elecom>
2020-04-11 16:29:37 +0900

    change color thema

--- inits/01_color.el
+++ inits/01_color.el
@@ -1,14 +1,4 @@
 ;; カラー設定
 
-(use-package monokai-theme
-  :config
-  (load-theme 'monokai t)
+(load-theme 'wombat t)
 
-  ;; ;; 透過
-  ;; (defun set-alpha (alpha-num)
-  ;;   "set frame parameter 'alpha"
-  ;;   (interactive "nAlpha: ")
-  ;;   (set-frame-parameter nil 'alpha (cons alpha-num '(100))))
-  ;; ;; 初期値
-  ;; (set-frame-parameter nil 'alpha 100)
-)
